e9dc39a7d51654ee8108be8ceb79d9013bbeb0aa
Nicky Semenza <nicky@nickysemenza.com>
2017-05-13 23:38:27 -0700

    Styling thread list

--- frontend/src/pages/Inbox/ThreadItem.js
+++ frontend/src/pages/Inbox/ThreadItem.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment'
 export default class ThreadItem extends Component {
-
     render () {
         let thread = this.props.thread;
         let date = moment.utc(thread.snippet.created_at,'YYYY-MM-DD HH:mm:ss').local();
@@ -13,11 +12,23 @@ export default class ThreadItem extends Component {
         return(
             <div className={style} key={thread.id}>
                 <div className="inbox-thread-list-item">
-                    <strong>#{thread.id}:</strong> {thread.snippet.subject}
-                    <br/>
-                    {thread.snippet.sender}
-                    <br/>
-                    {date.fromNow()} // {date.calendar()}
+                    <div>
+                        <div className="state-icon-assigned">aa</div>
+                    </div>
+                    <div>
+                        <div className="threaditem-row">
+                            <div className="threaditem-rowitem-left sender">{thread.snippet.sender}</div>
+                            <div className="threaditem-rowitem-right time">{date.calendar()}</div>
+                        </div>
+                        <div className="threaditem-row">
+                            <div className="threaditem-rowitem-left subject">{thread.snippet.subject}</div>
+                            <div className="threaditem-rowitem-right assign">Person1, Person2, Person3, Person4</div>
+                        </div>
+                        <div className="threaditem-row">
+                            <div className="threaditem-rowitem-left">{thread.snippet.body_plain}</div>
+                        </div>
+                    </div>
+
                     <br/>
                 </div>
             </div>);
