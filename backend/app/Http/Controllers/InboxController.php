46aea2a27f1f440278d8150878f03f6196cda929
MaxLin <maxylin@gmail.com>
2017-07-15 22:07:57 -0700

    Remove extra spaces

--- backend/app/Http/Controllers/InboxController.php
+++ backend/app/Http/Controllers/InboxController.php
@@ -284,7 +284,7 @@ class InboxController extends Controller
         } elseif ($action === 'delete') {
             $user->recordThreadEvent($draft->thread, UserEvent::TYPE_DELETE_DRAFT);
             $draft->delete();
-        }        
+        }
 
         return response()->success($draft);
     }
