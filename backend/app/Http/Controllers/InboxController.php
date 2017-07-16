9bbe719e592d91c2798eb2896dcffa706bb46a78
MaxLin <maxylin@gmail.com>
2017-07-15 22:07:02 -0700

    Change else if to elseif

--- backend/app/Http/Controllers/InboxController.php
+++ backend/app/Http/Controllers/InboxController.php
@@ -283,10 +283,7 @@ class InboxController extends Controller
         } elseif ($action === 'delete') {
             $user->recordThreadEvent($draft->thread, UserEvent::TYPE_DELETE_DRAFT);
             $draft->delete();
-        } else if ($action === 'delete'){
-            $user->recordThreadEvent($draft->thread, UserEvent::TYPE_DELETE_DRAFT);
-            $draft->delete();
-        }        
+        }      
 
         return response()->success($draft);
     }
